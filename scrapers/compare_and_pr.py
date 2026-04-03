a9c19429c72d47b191d1f5decaad28dd072cd89b
itsJustinCase <aymen.boulmedais@gmail.com>
2026-04-03 12:21:17 +0200

    Update compare_and_pr.py

--- scrapers/compare_and_pr.py
+++ scrapers/compare_and_pr.py
@@ -49,6 +49,7 @@ CSV_FILES = {
     "sweden.csv":      "sweden.csv",
     "UK.csv":          "UK.csv",
     "romania.csv":     "romania.csv",
+    "romania_blacklist.csv":     "romania_blacklist.csv",
 }
 
 GITHUB_API  = "https://api.github.com"
